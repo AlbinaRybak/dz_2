4038ead76d383c57a0b2a93d954cc9fbc7de7e59
Albina <albinarybak13.09@gmail.com>
2023-06-04 18:16:34 +0300

    first commit

--- dz 2.py
+++ dz 2.py
@@ -1,2 +1,11 @@
-digit= input('Type 5x:')
-print(1,2,3,4,5,sep='\n')
+number= input ("Type 4x:")
+
+digit1= int(number) // 1000
+digit2= (int(number) // 100) % 10
+digit3= (int(number) // 10) % 10
+digit4= int(number) % 10
+
+print(digit1)
+print(digit2)
+print(digit3)
+print(digit4)
