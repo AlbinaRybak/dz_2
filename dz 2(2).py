f696506f23dbf953c8a4e9812e1085d242dbecdf
Albina <albinarybak13.09@gmail.com>
2023-06-04 18:16:43 +0300

    second commit

--- dz 2(2).py
+++ dz 2(2).py
@@ -1,3 +1,11 @@
-a=input("Type 5x:")
+n1 = int (input("Ввведіть 5-ти значне число"))
 
-print(a[::-1])
+n2 = 0
+
+while n1 > 0:
+    digit = n1 % 10
+    n1 = n1 // 10
+
+    n2 = n2 * 10 + digit
+
+print(n2)
