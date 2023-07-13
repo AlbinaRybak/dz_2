b600321b032c06f5822883703d0f496dcc6c38b6
Albina <albinarybak13.09@gmail.com>
2023-07-13 20:04:01 +0300

    first commit

--- 32.py
+++ 32.py
@@ -8,6 +8,7 @@ class Item:
     def __str__(self):
         return f"{self.name}, price: {self.price}"
 
+
 class User:
     def __init__(self, name, surname, numberphone):
         self.name = name
@@ -17,13 +18,15 @@ class User:
     def __str__(self):
         return f"{self.name} {self.surname}"
 
+
 class Purchase:
     def __init__(self, user):
-        self.user = user
         self.products = {}
+        self.user = user
+        self.total = 0
 
-    def add_item(self, item, count):
-        self.products[item] = count
+    def add_item(self, item, cnt):
+        self.products[item] = cnt
 
     def __str__(self):
         result = f"User: {self.user}\nItems:\n"
@@ -37,13 +40,38 @@ class Purchase:
             total += item.price * count
         return total
 
+
 lemon = Item('lemon', 5, "yellow", "small")
 apple = Item('apple', 2, "red", "middle")
+print(lemon)  # lemon, price: 5
 
-print(lemon)
+buyer = User("Ivan", "Ivanov", "02628162")
+print(buyer)  # Ivan Ivanov
 
-buyer = User("Kirill", "Costin", "02628162")
-print(buyer)
+cart = Purchase(buyer)
+cart.add_item(lemon, 4)
+cart.add_item(apple, 20)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 20 pcs.
+"""
+assert isinstance(cart.user, User) is True, 'Екземпляр класу User'
+assert cart.get_total() == 60, "Всього 60"
+assert cart.get_total() == 60, 'Повинно залишатися 60!'
+cart.add_item(apple, 10)
+print(cart)
+"""
+User: Ivan Ivanov
+Items:
+lemon: 4 pcs.
+apple: 10 pcs.
+"""
+
+assert cart.get_total() == 40
+
 
 cart = Purchase(buyer)
 cart.add_item(lemon, 4)
